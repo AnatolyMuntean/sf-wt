6c0b9c05478f6b7c528ed5ae4d4c76ee4a214cde
AM <anmuntean@pentalog.com>
2017-11-14 17:18:20 +0200

    Bootstrap CDN, some template corrections.

--- src/AppBundle/DataFixtures/ORM/TanksFixtures.php
+++ src/AppBundle/DataFixtures/ORM/TanksFixtures.php
@@ -12,18 +12,24 @@ class TanksFixtures extends Fixture
     public function load(ObjectManager $manager)
     {
         $tanks = [
-            'Panzer II' => '2 cm KwK 30',
-            'Tiger I' => 'KwK 36',
-            'Tiger II' => 'KwK 43 L71'
+            'Panzer II' => ['2 cm KwK 30'],
+            'Tiger I' => ['KwK 36'],
+            'Tiger II' => ['KwK 43 L71'],
+            'Maus' => [
+                '7.5 cm KwK 37',
+                '12.8 cm Pak 44',
+            ],
         ];
 
-        foreach ($tanks as $tankName => $gunName) {
+        foreach ($tanks as $tankName => $gunNames) {
             $tankEntity = new Tank();
             $tankEntity->setName($tankName);
 
-            /** @var Gun $gunEntity */
-            $gunEntity = $this->getReference($gunName);
-            $tankEntity->addGun($gunEntity);
+            foreach ($gunNames as $gunName) {
+                /** @var Gun $gunEntity */
+                $gunEntity = $this->getReference($gunName);
+                $tankEntity->addGun($gunEntity);
+            }
 
             $manager->persist($tankEntity);
         }
