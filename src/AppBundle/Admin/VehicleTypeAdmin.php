452d2268fd6daaca3bc042e0b9951194fbe7540d
AM <anmuntean@pentalog.com>
2017-11-21 15:40:15 +0200

    Admin bundle along with generic admin classes.

---
+++ src/AppBundle/Admin/VehicleTypeAdmin.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace AppBundle\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class VehicleTypeAdmin extends AbstractAdmin
+{
+    protected function configureFormFields(FormMapper $form)
+    {
+        $form
+            ->add('name', 'text')
+            ->add('abbreviation', 'text', [
+                'required' => false
+            ])
+            ->add('description', 'textarea', [
+                'required' => false
+            ]);
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $filter)
+    {
+        $filter
+            ->add('name')
+            ->add('abbreviation');
+    }
+
+    protected function configureListFields(ListMapper $list)
+    {
+        $list
+            ->addIdentifier('name')
+            ->addIdentifier('abbreviation');
+    }
+}
---
+++ src/AppBundle/Admin/VendorAdmin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace AppBundle\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class VendorAdmin extends AbstractAdmin
+{
+    protected function configureFormFields(FormMapper $form)
+    {
+        $form->add('country');
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $filter)
+    {
+        $filter->add('country');
+    }
+
+    protected function configureListFields(ListMapper $list)
+    {
+        $list->addIdentifier('country');
+    }
+}
