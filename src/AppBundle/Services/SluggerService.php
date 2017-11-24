0a45f19f907e33c879a3d63ae9704d26d29d193e
AM <anmuntean@pentalog.com>
2017-11-24 12:28:05 +0200

    Tank template corrections and slug generation improvement.

--- src/AppBundle/Services/SluggerService.php
+++ src/AppBundle/Services/SluggerService.php
@@ -9,9 +9,9 @@ class SluggerService
         $input = mb_strtolower($input);
 
         if (function_exists('iconv')) {
-            $input = iconv("UTF-8", "ISO-8859-1//TRANSLIT", $input);
+            $input = iconv("UTF-8", "ascii//TRANSLIT", $input);
         }
 
-        return trim(preg_replace('/\W|\s+/', '-', $input));
+        return trim(preg_replace('/\W|\s+/', '-', $input), ' \t\n\r\0\x0B-');
     }
 }
